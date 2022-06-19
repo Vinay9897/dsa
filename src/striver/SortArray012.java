b9164919c4f1ddba84fb7b70f7207182d72713d2
Vinay Yadav <vinayyadav91190@gmail.com>
2022-06-20 00:39:54 +0530

    sory the array in O(n)

--- src/striver/SortArray012.java
+++ src/striver/SortArray012.java
@@ -1,35 +1,44 @@
 package striver;
 
-import java.util.ArrayList;
-
 public class SortArray012 {
     public static void main(String[] args) {
         int[] arr = { 0, 1, 2, 2, 1, 0 };
-        sort(arr, arr.length);
-        for (int valu : arr) {
-            System.out.print(valu + " ");
+        int maxi = Integer.MIN_VALUE;
+        maxi = findMax(arr, maxi);
+        sort(arr, arr.length, maxi);
+        for (int val : arr) {
+            System.out.print(val + " ");
         }
+
     }
 
-    private static void sort(int[] arr, int n) {
-        int t = arr.length - 1;
+    private static int findMax(int[] arr, int maxi) {
         for (int i = 0; i < arr.length; i++) {
+            maxi = Math.max(maxi, arr[i]);
+        }
+        return maxi;
+    }
 
-            if (arr[i] == 2 && arr[t] != 2) {
-                swap(i, t, arr);
-                t--;
-            } else if (arr[i] == 2 && arr[t] == 2) {
-                while (t >= 0) {
+    private static void sort(int[] arr, int n, int size) {
+        int[] count = new int[size + 1];
+        for (int i = 0; i < n; i++) {
+            count[arr[i]]++;
+        }
 
-                }
+        int[] sortedArray = new int[n];
+        int p = 0;
+        int i = 0;
+        while (p < count.length) {
+            if (count[p] == 0) {
+                p++;
+            } else {
+                sortedArray[i++] = p;
+                count[p]--;
             }
         }
+        int j = 0;
+        for (int val : sortedArray) {
+            arr[j++] = val;
+        }
     }
-
-    private static void swap(int a, int b, int[] arr) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-    }
-
 }
