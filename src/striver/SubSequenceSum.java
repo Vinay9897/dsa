9fbb9a932a35725fd3311a383d92f1faae37d16b
Vinay Yadav <vinayyadav91190@gmail.com>
2022-07-14 23:50:34 +0530

    execute the prog of subsequence element==target

---
+++ src/striver/SubSequenceSum.java
@@ -0,0 +1,30 @@
+package striver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+
+class SubSequenceSum {
+    static HashSet<List<Integer>> result = new HashSet<List<Integer>>();
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int[] arr = { 1, 2, 3, 3 };
+        int sum = 6;
+        findSumValue(0, arr, list, sum);
+        System.out.print(result);
+    }
+
+    private static void findSumValue(int index, int[] arr, ArrayList<Integer> list, int sum) {
+        if (index >= arr.length) {
+            if (sum == 0) {
+                result.add(new ArrayList<Integer>(list));
+            }
+            return;
+        }
+        list.add(arr[index]);
+        findSumValue(index + 1, arr, list, sum - arr[index]);
+        list.remove(list.size() - 1);
+        findSumValue(index + 1, arr, list, sum);
+    }
+}
