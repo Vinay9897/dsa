698dedc8074f3d3628890171a31cc80e91693d47
Vinay Yadav <vinayyadav91190@gmail.com>
2022-07-14 19:28:51 +0530

    Find the all subsequence program executed

---
+++ src/FindSubSequence.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindSubSequence {
+    static List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] arr = { 3, 1, 2 };
+        subsequence(arr, 0, list);
+        System.out.println(result);
+    }
+
+    private static void subsequence(int[] arr, int i, List<Integer> list) {
+
+        if (i >= arr.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        list.add(arr[i]);
+        subsequence(arr, i + 1, list); // take
+        list.remove(list.size() - 1); // to remove the last element from the list
+        subsequence(arr, i + 1, list); // not take
+    }
+}
