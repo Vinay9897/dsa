75559554895135700d1dba88c59a8fae30bafa8c
Vinay Yadav <vinayyadav91190@gmail.com>
2022-07-15 22:48:47 +0530

    comment the information

--- src/striver/SubSetSum.java
+++ src/striver/SubSetSum.java
@@ -13,15 +13,30 @@ public class SubSetSum {
         System.out.println(list);
     }
 
+    // in this problem we have to print the sum of all subsets in sorted sort
+    /*
+     * ex: arr = {1,2,3}
+     * no of possible subset ---> 2 ki power (array ki length)
+     * [] = 0
+     * [1] = 1
+     * [2] = 2
+     * [3] = 3
+     * [1,2] = 3
+     * [1,2,3] = 6
+     * [1,3] = 4
+     * [2,3] = 5
+     */
     private static void findSum(List<Integer> list, int[] arr, int sum, int index) {
+        // when index equal to array length store the sum in the list and return back
+        // the funtion
         if (index >= arr.length) {
             list.add(sum);
             return;
         }
-        // pick the element
+        // this loop for picking the element
         findSum(list, arr, sum + arr[index], index + 1);
 
-        // not pick the element
+        // this loop for not picking the element
         findSum(list, arr, sum, index + 1);
 
     }
