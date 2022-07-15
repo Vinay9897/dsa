cc475bc28bbcb696c9ad47aa3cdfaf192892a37a
Vinay Yadav <vinayyadav91190@gmail.com>
2022-07-15 19:34:03 +0530

    CombinationSum program executed

---
+++ src/striver/CombinationSum2.java
@@ -0,0 +1,32 @@
+package striver;
+
+import java.util.*;
+import java.util.List;
+
+public class CombinationSum2 {
+    public static void main(String[] args) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        int[] arr = { 2, 3, 5, 6, 7 };
+        int target = 7;
+        findSum(result, list, arr, target, 0);
+        System.out.print(result);
+    }
+
+    private static void findSum(List<List<Integer>> result, List<Integer> list, int[] arr, int target, int i) {
+        if (i >= arr.length) {
+            if (target == 0) {
+                result.add(new ArrayList<>(list));
+                return;
+            } else
+                return;
+        }
+        if (target >= arr[i]) {
+            list.add(arr[i]);
+            findSum(result, list, arr, target - arr[i], i + 1);
+            list.remove(list.size() - 1);
+        }
+        findSum(result, list, arr, target, i + 1);
+    }
+
+}
