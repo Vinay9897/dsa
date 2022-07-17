c6f313968fd98168208620fb62af76b95bdd29c4
Vinay Yadav <vinayyadav91190@gmail.com>
2022-07-17 15:23:08 +0530

    herosvillian logical error removed

--- src/infosis/HerosVillan.java
+++ src/infosis/HerosVillan.java
@@ -1,46 +1,40 @@
 package infosis;
 
+import java.util.Arrays;
+
 public class HerosVillan {
     public static void main(String[] args) {
         int[] heros = { 3, 3, 3 };
-        int[] villian = { 4, 4, 4, 4 };
+        int[] villian = { 1, 4, 2, 4, 3 };
+        Arrays.sort(villian);
         int remainVillian = 0;
         int i = 0;
         int j = 0;
-        while (heros.length > 0) {
+        while (i < villian.length && j < heros.length) {
 
-            while (i < villian.length) {
-
-                if (heros[j] > villian[i]) {
-                    heros[j] -= villian[i];
-                    i++;
-                    if (i >= villian.length) {
-                        break;
-                    }
-                } else if (heros[j] < villian[i]) {
-                    remainVillian = villian.length - i;
-                    j++;
-                    if (j >= heros.length) {
-                        break;
-                    }
-                    if (i >= villian.length) {
-                        break;
-                    }
-                } else {
-                    j++;
-                    i++;
-                    remainVillian = villian.length - i - 1;
-                    if (j >= heros.length) {
-                        break;
-                    }
-                    if (i >= villian.length) {
-                        break;
-                    }
-
+            if (heros[j] > villian[i]) {
+                heros[j] -= villian[i];
+                i++;
+                if (i >= villian.length || j >= heros.length) {
+                    break;
                 }
+            } else if (heros[j] < villian[i]) {
+                remainVillian = villian.length - i;
+                j++;
+                if (j >= heros.length || i >= villian.length) {
+                    break;
+                }
+            } else {
+                remainVillian = villian.length - i;
+                j++;
+                i++;
+                if (j >= heros.length || i >= villian.length) {
+                    break;
+                }
+
             }
-            System.out.println("No of villian :" + remainVillian);
         }
+        System.out.println("No of villian :" + remainVillian);
     }
 
 }
