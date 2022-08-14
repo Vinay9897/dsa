98cd54c8284a5c1cfaa9ca33445c47d9538015fc
Vinay Yadav <vinayyadav91190@gmail.com>
2022-08-14 14:02:09 +0530

    half logic is correct

--- src/dsaQuestion/ConstructSmallestNo.java
+++ src/dsaQuestion/ConstructSmallestNo.java
@@ -10,11 +10,49 @@ Return the lexicographically smallest possible string num that meets the conditi
 */
 package dsaQuestion;
 
-import java.util.*;
+// import java.util.*;
 
 public class ConstructSmallestNo {
     public static void main(String[] args) {
-        String pattern = "IIIDIDDD";
-        Scanner
+        String pattern = "DDDD ";
+        helper(pattern);
+    }
+
+    public static void helper(String pattern) {
+        int i = 0;
+        int s = 0;
+        String str = "";
+        int flag = 0;
+        while (i < pattern.length() - 1) {
+            if (flag == 0) {
+                s = i;
+            }
+            flag = 1;
+            // for "I"
+            if (pattern.charAt(i) == 'I' && pattern.charAt(i + 1) == 'I') {
+
+            } else {
+                if (pattern.charAt(i) != 'D') {
+                    for (int s1 = s + 1; s1 <= i + 1; s1++) {
+                        str += s1;
+                    }
+                    flag = 0;
+                }
+            }
+
+            // for "D"
+            if (pattern.charAt(i) == 'D' && pattern.charAt(i + 1) == 'D') {
+
+            } else {
+                if (pattern.charAt(i) != 'I') {
+                    for (int s1 = i + 2; s1 > s + 1; s1--) {
+                        str += s1;
+                    }
+                    flag = 0;
+                }
+            }
+            i++;
+        }
+        System.out.println(str);
     }
 }
